4176cc8400d5ce41b37dce593fcd786c2af2d6e2
cyxabima <ukashaanwerali@gmail.com>
2025-09-28 12:17:33 +0500

    error is interface in go

---
+++ errors-in-.go
@@ -0,0 +1,23 @@
+package main
+
+// In Go Error is an interface with only one method Error () string
+
+
+
+
+
+/*
+/----------------------/
+In JS or in python
+/---------------------/
+try {
+	const user = getUser() // ~ dangerous function
+	const profile = getProfile(user.id) // also dangerous but i do not error is raise by which function if i separate error handling i can find out which function is throwing an error but we will stuck in nested try catch block so no body use that unless needed.
+}
+	catch(error){
+	console.log (error)
+}
+
+
+and also if you do not know which is dangerous function you can't tell just by looking at function signature so you don't care about which function is actually dangerous you put them in try catch blok and simply move on 
+*/
